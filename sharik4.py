68fc45cf31079b48d35dc18cf7b3a6b4b057c970
Mayyyyyyyyyyyyyy <forduy96@gmail.com>
2023-12-06 21:44:47 +0200

    Co-authored-by: Nikita Fordui <nikita.forduy@protonmail.com>

---
+++ sharik4.py
@@ -0,0 +1,16 @@
+
+#a > b та str1 та str2 однакові - надрукуй Єєєєєє рок
+#інакше a < b та строчки різні - надрукуй  Не єєє рок
+#якщо все це не виконається - надрукуй Крінж
+Andrey = 18 
+Sanya = 28
+str1 = "antonvatnik"
+str2 = "antonvatnik"
+
+if a > b and str1 == str2:
+    print("ARRRRRRRRR")
+else:
+    if a < b and str1 != str2: 
+        print ("NEEEE ROKKKK")
+    else:
+        print("ALAKHUA AKBAR")
